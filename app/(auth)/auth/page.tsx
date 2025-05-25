d1841cf0406329851e129c6a730e560335d71dea
RaveendraGuthula <122366955+RaveendraGuthula@users.noreply.github.com>
2025-05-25 09:33:40 +0530

    installed shadcn and auth page added

---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthPage() {
+  return (
+    <div className="text-blue-500 text-2xl flex items-center justify-center h-screen">
+      <h1>Welcome to the Auth Page</h1>
+    </div>
+  );
+}
