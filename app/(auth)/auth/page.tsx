0c3104f772b06dcfbf8aeeca2aa2b0754866caa3
RaveendraGuthula <122366955+RaveendraGuthula@users.noreply.github.com>
2025-05-25 10:10:23 +0530

    implemented the basic functionality for sign in/sign up

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function AuthPage() {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const router = useRouter();
+
+  function toggleAuthMode() {
+    setIsSignUp(!isSignUp);
+  }
   return (
-    <div className="text-blue-500 text-2xl flex items-center justify-center h-screen">
-      <h1>Welcome to the Auth Page</h1>
+    <div className="flex min-h-screen mt-7">
+      <div className="w-full max-w-3xl mx-auto flex flex-col lg:flex-row p-3">
+        <div>
+          <div
+            className="mb-8 lg:mb-12 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <ChevronLeft className="text-gray-500 h-6 w-6 sm:h-8 sm:w-8 border-2 rounded-full p-1" />
+          </div>
+          <div>
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3">
+              {isSignUp ? "Sing Up" : "Sing In"}
+            </h2>
+            <p className="text-base sm:text-lg lg:text-xl text-gray-400 mb-6 sm:mb-8">
+              {isSignUp
+                ? "Join SahandStore today and discover exclusive deals on your favorite products!"
+                : "Welcome back to SahandStore! Log in to continue your shopping journey."}
+            </p>
+          </div>
+          <div className="mt-4 sm:mt-5 flex items-center justify-center">
+            <p className="text-base sm:text-lg lg:text-xl text-gray-600">
+              {isSignUp
+                ? "Already have an account? "
+                : "Don't have an account? "}
+            </p>
+            <Button
+              variant="link"
+              className="text-base sm:text-xl lg:text-2xl text-gray-500 cursor-pointer"
+              onClick={toggleAuthMode}
+            >
+              {isSignUp ? "Sign In" : "Sign Up"}
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
