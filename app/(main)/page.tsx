ab478c9b367510d961203168c270e28b1edbde28
RaveendraGuthula <122366955+RaveendraGuthula@users.noreply.github.com>
2025-05-25 09:10:04 +0530

    changed file structure according project

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-red-500 text-2xl flex items-center justify-center h-screen">
+      <h1>Welcome to the E-commerce App</h1>
+    </div>
+  );
+}
