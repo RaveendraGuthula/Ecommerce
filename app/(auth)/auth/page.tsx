699cf2751bb2ab17d4ee3d40ebf9ab967dc0ff36
RaveendraGuthula <122366955+RaveendraGuthula@users.noreply.github.com>
2025-05-28 16:20:01 +0530

    Refactor AuthForm component: reorganize imports, enhance loading state handling, and improve error messaging

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { ChevronLeft, Loader2 } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { FormEvent, useEffect, useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { getLoginFormData, handleLoginSubmit } from "@/actions/auth/login";
-import { getSignupFormData , handleSignupSubmit  } from "@/actions/auth/signup";
-import { toast } from "sonner";
-import { IAttributes } from "oneentry/dist/base/utils";
+import { useState, useEffect, FormEvent, Suspense } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { ChevronLeft, Loader2 } from 'lucide-react';
+import { getSignupFormData, handleSignupSubmit } from '@/actions/auth/signup';
+import { getLoginFormData, handleLoginSubmit } from '@/actions/auth/login';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { toast } from 'sonner';
+import { IAttributes } from 'oneentry/dist/base/utils';
 
 interface SignUpFormData {
   email: string;
@@ -22,33 +22,39 @@ interface LoginFormData {
   password: string;
 }
 
-export default function AuthPage() {
-  const [isSignUp, setIsSignUp] = useState(false);
+function AuthForm() {
+  const [isSignUp, setIsSignUp] = useState(true);
+
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const [formData, setFormData] = useState<IAttributes[]>([]);
   const [inputValues, setInputValues] = useState<
     Partial<SignUpFormData & LoginFormData>
   >({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [error, setError] = useState<string | null>("Not valid");
+  const [error, setError] = useState<string | null>('Not valid');
 
   useEffect(() => {
-    const type = searchParams.get("type");
-    setIsSignUp(type !== "login");
+    const type = searchParams.get('type');
+    setIsSignUp(type !== 'login');
   }, [searchParams]);
 
   useEffect(() => {
     setIsLoading(true);
     setError(null);
     const fetchData = isSignUp ? getSignupFormData : getLoginFormData;
-
     fetchData()
       .then((data) => setFormData(data))
-      .catch(() => setError("Failed to load form data. Please try again."))
+      .catch((err) => setError(`Failed to fetch form data: ${err}`))
       .finally(() => setIsLoading(false));
   }, [isSignUp]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -62,95 +68,99 @@ export default function AuthPage() {
             inputValues as SignUpFormData
           );
 
-          if ("identifier" in response) {
+          if ('identifier' in response) {
             setInputValues({});
             setIsSignUp(false);
-            toast("User has been created", {
-              description: "Please check you credentials to log in.",
+            toast('User has been created', {
+              description: 'Please enter your credentials to log in.',
               duration: 5000,
             });
           } else {
-            setError("Please fill out all required fields.");
+            setError(response.message);
           }
+        } else {
+          setError('Please fill out all required fields.');
         }
       } else {
         if (inputValues.email && inputValues.password) {
           const response = await handleLoginSubmit(
             inputValues as LoginFormData
           );
-
           if (response.message) {
             setError(response.message);
           }
         } else {
-          setError("Please fill out all required fields.");
+          setError('Please fill out all required fields.');
         }
       }
-    } catch (error) {
+    } catch (err) {
       setError(
-        error instanceof Error
-          ? error.message
-          : "An unexpected error occurred. Please try again."
+        err instanceof Error
+          ? err.message
+          : 'An error occurred. Please try again.'
       );
     } finally {
       setIsSubmitting(false);
     }
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setInputValues((prev) => ({ ...prev, [name]: value }));
-  };
-
-  function toggleAuthMode() {
+  // Toggles between signup and login forms.
+  const toggleForm = () => {
     setIsSignUp(!isSignUp);
     setError(null);
-    setInputValues({}); // Reset input values when toggling auth mode
-  }
+    setInputValues({});
+  };
 
   return (
-    <div className="flex min-h-screen mt-7">
-      <div className="w-full max-w-3xl mx-auto flex flex-col lg:flex-row p-3">
+    <div className='flex min-h-screen mt-7'>
+      <div className='w-full max-w-3xl mx-auto flex flex-col lg:flex-row p-3'>
         <div>
+          {/* Back button */}
           <div
-            className="mb-8 lg:mb-12 cursor-pointer"
-            onClick={() => router.push("/")}
+            className='mb-8 lg:mb-12 cursor-pointer'
+            onClick={() => router.push('/')}
           >
-            <ChevronLeft className="text-gray-500 h-6 w-6 sm:h-8 sm:w-8 border-2 rounded-full p-1" />
+            <ChevronLeft className='text-gray-500 h-6 w-6 sm:h-8 sm:w-8 border-2 rounded-full p-1' />
           </div>
+
+          {/* Form header */}
           <div>
-            <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3">
-              {isSignUp ? "Sing Up" : "Sing In"}
+            <h2 className='text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3'>
+              {isSignUp ? 'Sign Up' : 'Sign In'}
             </h2>
-            <p className="text-base sm:text-lg lg:text-xl text-gray-400 mb-6 sm:mb-8">
+            <p className='text-base sm:text-lg lg:text-xl text-gray-400 mb-6 sm:mb-8'>
               {isSignUp
-                ? "Join SahandStore today and discover exclusive deals on your favorite products!"
-                : "Welcome back to SahandStore! Log in to continue your shopping journey."}
+                ? 'Join SahandStore today and discover exclusive deals on your favorite products!'
+                : 'Welcome back to SahandStore! Log in to continue your shopping journey.'}
             </p>
           </div>
+
+          {/* Form and loading */}
+
           {isLoading ? (
-            <div className="flex justify-center items-center h-64">
-              <Loader2 className="h-8 w-8 animate-spin text-purple-500" />
+            <div className='flex justify-center items-center h-64'>
+              <Loader2 className='h-8 w-8 animate-spin text-purple-500' />
             </div>
           ) : (
-            <form className="space-y-4 sm:space-y-6" onSubmit={handleSubmit}>
-              {formData.map((field: any) => (
+            <form className='space-y-4 sm:space-y-6' onSubmit={handleSubmit}>
+              {formData.map((field) => (
                 <div key={field.marker}>
                   <Label
                     htmlFor={field.marker}
-                    className="text-base sm:text-lg text-gray-400 mb-1 sm:mb-2 block"
+                    className='text-base sm:text-lg text-gray-400 mb-1 sm:mb-2 block'
                   >
                     {field.localizeInfos.title}
                   </Label>
                   <Input
                     id={field.marker}
-                    type={field.marker === "password" ? "password" : "text"}
+                    type={field.marker === 'password' ? 'password' : 'text'}
                     name={field.marker}
-                    className="text-base sm:text-lg p-4 sm:p-6"
+                    className='text-base sm:text-lg p-4 sm:p-6'
                     placeholder={field.localizeInfos.title}
                     value={
-                      inputValues[field.marker as keyof typeof inputValues] ||
-                      ""
+                      inputValues[
+                        field.marker as keyof (SignUpFormData & LoginFormData)
+                      ] || ''
                     }
                     onChange={handleInputChange}
                     disabled={isSubmitting}
@@ -159,41 +169,49 @@ export default function AuthPage() {
               ))}
 
               {error && (
-                <div className="text-red-500 text-center mt-2">{error}</div>
+                <div className='text-red-500 mt-2 text-center'>{error}</div>
               )}
+
               <div>
                 <Button
-                  className="w-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:from-purple-600 hover:via-pink-600 hover:to-red-600 text-white text-base sm:text-xl font-bold p-4 sm:p-6 rounded-md shadow-xl transition-colors duration-300 ease-in-out cursor-pointer"
+                  className='w-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:from-purple-600 hover:via-pink-600 hover:to-red-600 text-white text-base sm:text-xl font-bold p-4 sm:p-6 rounded-md shadow-xl transition-colors duration-300 ease-in-out cursor-pointer'
                   disabled={isSubmitting}
                 >
                   {isSubmitting ? (
-                    <Loader2 className="h-5 w-5 sm:h-6 sm:w-6 animate-spin" />
+                    <Loader2 className='h-5 w-5 sm:h-6 sm:w-6 animate-spin' />
                   ) : isSignUp ? (
-                    "sign Up"
+                    'Sign Up'
                   ) : (
-                    "Sign In"
+                    'Sign In'
                   )}
                 </Button>
               </div>
             </form>
           )}
 
-          <div className="mt-4 sm:mt-5 flex items-center justify-center">
-            <p className="text-base sm:text-lg lg:text-xl text-gray-600">
-              {isSignUp
-                ? "Already have an account? "
-                : "Don't have an account? "}
+          {/* Toggle form */}
+          <div className='mt-4 sm:mt-5 flex items-center justify-center'>
+            <p className='text-base sm:text-lg lg:text-xl text-gray-600'>
+              {isSignUp ? 'Already a member?' : "Don't have an account?"}
             </p>
             <Button
-              variant="link"
-              className="text-base sm:text-xl lg:text-2xl text-gray-500 cursor-pointer"
-              onClick={toggleAuthMode}
+              variant='link'
+              className='text-lg sm:text-xl lg:text-2xl text-gray-500 cursor-pointer'
+              onClick={toggleForm}
             >
-              {isSignUp ? "Sign In" : "Sign Up"}
+              {isSignUp ? 'Sign in' : 'Sign up'}
             </Button>
           </div>
         </div>
       </div>
     </div>
+  );
+}
+
+export default function Component() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthForm />
+    </Suspense>
   );
 }
