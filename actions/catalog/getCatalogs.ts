0798ab2e7c948ab3c68e41672779a48d882c5a87
RaveendraGuthula <122366955+RaveendraGuthula@users.noreply.github.com>
2025-05-28 12:29:16 +0530

    Implement product catalog functionality with data fetching and display

---
+++ actions/catalog/getCatalogs.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { fetchApiclient } from '@/lib/oneentry';
+import { IPagesEntity } from 'oneentry/dist/pages/pagesInterfaces';
+
+export const getCatalogs = async (): Promise<IPagesEntity[]> => {
+  try {
+    const apiClient = await fetchApiclient();
+    const pages = await apiClient?.Pages.getRootPages('en_US');
+
+    const catalogPages = pages?.filter(
+      (page: { type: string }) => page.type === 'catalog_page'
+    );
+
+    // Always return an array
+    return catalogPages?.length ? catalogPages : [];
+  } catch (error) {
+    console.error({ error });
+    return []; // In case of error, return an empty array
+  }
+};
